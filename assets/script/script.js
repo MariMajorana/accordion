41cc0ba32dfcc4eafb3fdbdb9ba0632af0f6c248
MariMajorana <130094509+MariMajorana@users.noreply.github.com>
2024-09-17 15:51:53 -0300

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,40 @@
+const answer1 = document.getElementById("answer1")
+function quest1(){
+    if(answer1.style.display === "none"){
+        answer1.style.display = "block" 
+    }else{
+        answer1.style.display = "none"
+    }
+}
+const answer2 = document.getElementById("answer2")
+function quest2(){
+    if(answer2.style.display === "none"){
+        answer2.style.display = "block" 
+    }else{
+        answer2.style.display = "none"
+    }
+}
+const answer3 = document.getElementById("answer3")
+function quest3(){
+    if(answer3.style.display === "none"){
+        answer3.style.display = "block" 
+    }else{
+        answer3.style.display = "none"
+    }
+}
+const answer4 = document.getElementById("answer4")
+function quest4(){
+    if(answer4.style.display === "none"){
+        answer4.style.display = "block" 
+    }else{
+        answer4.style.display = "none"
+    }
+}
+const answer5 = document.getElementById("answer5")
+function quest5(){
+    if(answer5.style.display === "none"){
+        answer5.style.display = "block" 
+    }else{
+        answer5.style.display = "none"
+    }
+}
