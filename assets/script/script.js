510c5892f18725c1eb95b99675735b45efc448c8
MariMajorana <130094509+MariMajorana@users.noreply.github.com>
2025-01-09 19:10:18 -0300

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,55 @@
+const answer1 = document.getElementById("answer1")
+const quests1 = document.getElementById("quests1")
+function quest1(){
+    if(answer1.style.display === "none"){
+        answer1.style.display = "block" 
+        quests1.style.fontWeight = 600
+    }else{
+        answer1.style.display = "none"
+        quests1.style.fontWeight = 400
+    }
+}
+const answer2 = document.getElementById("answer2")
+const quests2 = document.getElementById("quests2")
+function quest2(){
+    if(answer2.style.display === "none"){
+        answer2.style.display = "block"
+        quests2.style.fontWeight = 600 
+    }else{
+        answer2.style.display = "none"
+        quests2.style.fontWeight = 400
+    }
+}
+const answer3 = document.getElementById("answer3")
+const quests3 = document.getElementById("quests3")
+function quest3(){
+    if(answer3.style.display === "none"){
+        answer3.style.display = "block"
+        quests3.style.fontWeight = 600 
+    }else{
+        answer3.style.display = "none"
+        quests3.style.fontWeight = 400 
+    }
+}
+const answer4 = document.getElementById("answer4")
+const quests4 = document.getElementById("quests4")
+function quest4(){
+    if(answer4.style.display === "none"){
+        answer4.style.display = "block"
+        quests4.style.fontWeight = 600  
+    }else{
+        answer4.style.display = "none"
+        quests4.style.fontWeight = 400 
+    }
+}
+const answer5 = document.getElementById("answer5")
+const quests5 = document.getElementById("quests5")
+function quest5(){
+    if(answer5.style.display === "none"){
+        answer5.style.display = "block"
+        quests5.style.fontWeight = 600   
+    }else{
+        answer5.style.display = "none"
+        quests5.style.fontWeight = 400 
+    }
+}
